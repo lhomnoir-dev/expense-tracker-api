288836554d69d2de59fd1579b29de2ea61cc5f79
lhomnoir <lhomnoir@outlook.fr>
2026-08-04 19:46:16 +0000

    fix: feature get_db

--- app/database.py
+++ app/database.py
@@ -1,17 +1,17 @@
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import create_engine
 
-from .database import DATABASE_URL
-
+from .config import DATABASE_URL
 
 engine = create_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=True, bind=engine)
 
 Base = declarative_base()
 
+
 def get_db():
     try:
         db = SessionLocal()
         yield db
-    except:
+    finally:
         db.close()
